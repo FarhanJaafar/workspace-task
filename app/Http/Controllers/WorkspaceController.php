20ba62de43dba23385f0aa076b26eda4a823daeb
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-23 12:15:40 +0800

    Add: store function for workspace controller

--- app/Http/Controllers/WorkspaceController.php
+++ app/Http/Controllers/WorkspaceController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Workspace;
 use Illuminate\Http\Request;
 
 class WorkspaceController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        Workspace::create([
+            'user_id' =>auth()->user()->id,
+            'name' =>$request->name,
+            'datetime'=>$request->datetime,
+            'status'=>$request->status
+        ]);
+
+        return to_route('home');
+    }
+
+
 }
