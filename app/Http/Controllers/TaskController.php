9ee4099cc067a6809a8c277f8f6ba79e2f25bc2a
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-24 12:24:48 +0800

    Add: index function and edit store function

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,26 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use App\Models\Workspace;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function store(Request $request)
+    public function index()
     {
-        Task::create([
-            'workspace_id' =>auth()->user()->id,
-            'name' =>$request->name,
-            'datetime'=>$request->datetime,
-            'status'=>$request->status
-        ]);
-
-        return to_route('task.show');
+        return view('task.show');
     }
 
-    public function show(Workspace $workspace)
+    public function store(Request $request, Workspace $workspace)
     {
-        $tasks = $workspace->task;
-        return view('task.show', compact('workspace','tasks'));
+        Task::create([
+            'workspace_id' =>$workspace->id,
+            'name' =>$request->name,
+            'datetime'=>$request->datetime,
+            //'status'=>$request->status
+        ]);
+
+        return to_route('workspace.show', compact('workspace'));
     }
 }
