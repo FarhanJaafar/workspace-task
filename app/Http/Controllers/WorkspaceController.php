4601d3fbc51c5cd42aeb32fbeaead1563d4ed0ba
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-24 12:26:06 +0800

    Add: delete, show, edit n update function

--- app/Http/Controllers/WorkspaceController.php
+++ app/Http/Controllers/WorkspaceController.php
@@ -16,8 +16,38 @@ class WorkspaceController extends Controller
             'status'=>$request->status
         ]);
 
+
         return to_route('home');
     }
 
+    public function delete(Workspace $workspace)
+    {
+        $workspace->delete();
+
+        return to_route('home');
+    }
+
+    public function show(Workspace $workspace)
+    {
+        $tasks = $workspace->task;
+        return view('workspace.show', compact('workspace', 'tasks'));
+    }
+
+    public function edit(Workspace $workspace)
+    {
+        $workspace=Workspace::all();
+        return view('workspace.edit', compact('workspace'));
+    }
+
+    public function update(Request $request, Workspace $workspace)
+    {
+        $workspace->update([
+            'user_id' =>$workspace->user_id,
+            'name' =>$request->name,
+            'datetime'=>$request->datetime,
+            'status'=>$request->status
+        ]);
+        return to_route('home');
+    }
 
 }
