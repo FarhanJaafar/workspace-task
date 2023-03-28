ffcc775999f4849d11447b4283d8af77337ad950
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-28 13:16:29 +0800

    Add: uuid attribute in table db

--- database/migrations/2023_03_22_060019_create_workspaces_table.php
+++ database/migrations/2023_03_22_060019_create_workspaces_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users');
             $table->string('name');
+            $table->string('uuid');
             $table->datetime('datetime');
             $table->string('status');
             $table->timestamps();
