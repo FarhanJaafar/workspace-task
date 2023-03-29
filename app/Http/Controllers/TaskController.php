a02d9f258961970b8961320fa0f8d5252b9c2551
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-29 15:22:41 +0800

    Fix

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -29,19 +29,20 @@ class TaskController extends Controller
 
     public function delete(Workspace $workspace, Task $task)
     {
-        $this->authorize('delete', $task);
+        // $this->authorize('delete', $task);
         $task->delete();
         return to_route('workspace.show', compact('workspace'));
     }
 
     public function edit(Workspace $workspace, Task $task)
     {
-        $this->authorize('update', $workspace, $task);
+        // $this->authorize('update', $workspace, $task);
         return view('task.edit', compact('workspace', 'task'));
     }
 
     public function update(Request $request, Workspace $workspace, Task $task)
     {
+        // $this->authorize('update', $workspace, $task);
         $task->update([
             'name' =>$request->name,
             'datetime'=>$request->datetime,
