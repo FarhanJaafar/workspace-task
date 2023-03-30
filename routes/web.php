049e3d60f278f2349bc71d7db757d9a0f31e24cc
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-30 10:16:55 +0800

    Add: uuid in routes

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,10 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::post('/workspace/store', [App\Http\Controllers\WorkspaceController::class, 'store'])->name('workspace.store');
 Route::get('/workspace/show/{workspace:uuid}', [App\Http\Controllers\WorkspaceController::class, 'show'])->name('workspace.show');
 Route::get('/workspace/delete/{workspace}', [App\Http\Controllers\WorkspaceController::class, 'delete'])->name('workspace.delete');
-Route::get('/workspace/edit/{workspace}', [App\Http\Controllers\WorkspaceController::class, 'edit'])->name('workspace.edit');
+Route::get('/workspace/edit/{workspace:uuid}', [App\Http\Controllers\WorkspaceController::class, 'edit'])->name('workspace.edit');
 Route::post('/workspace/update/{workspace}', [App\Http\Controllers\WorkspaceController::class, 'update'])->name('workspace.update');
 
-
-//Route::get('/task/create', [App\Http\Controllers\TaskController::class, 'create'])->name('task.create');
 Route::post('/task/store/{workspace}', [App\Http\Controllers\TaskController::class, 'store'])->name('task.store');
 Route::get('/task/delete/{workspace}/{task}', [App\Http\Controllers\TaskController::class, 'delete'])->name('task.delete');
-Route::get('/task/edit/{workspace}/{task}', [App\Http\Controllers\TaskController::class, 'edit'])->name('task.edit');
+Route::get('/task/edit/{workspace:uuid}/{task}', [App\Http\Controllers\TaskController::class, 'edit'])->name('task.edit');
 Route::post('/task/update/{workspace}/{task}', [App\Http\Controllers\TaskController::class, 'update'])->name('task.update');
